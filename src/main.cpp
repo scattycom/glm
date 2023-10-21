4533dd0c963685e016807d66f9697c7d53f57f3c
scattycom <48954539+scattycom@users.noreply.github.com>
2023-10-21 21:46:32 +0800

    添加了vulkan和glfw支持

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
-#include<iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
+#include <iostream>
 
-int main()
-{
-	std::cout << 1 << std::endl;
+int main() {
+	// 初始化GLFW
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW." << std::endl;
+		return -1;
+	}
+
+	// 设置GLFW不使用OpenGL
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Demo", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// 初始化Vulkan
+	VkInstance instance;
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Vulkan Demo";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+		std::cerr << "Failed to create Vulkan instance." << std::endl;
+		return -1;
+	}
+
+	// 主循环
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	// 清理
+	vkDestroyInstance(instance, nullptr);
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
